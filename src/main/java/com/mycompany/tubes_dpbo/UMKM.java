faaf0d5c6fcc12b6edb4c62434262da8dec8d01d
nadya <152418392+theavrl@users.noreply.github.com>
2024-12-09 15:47:19 +0700

    Update UMKM.java

--- src/main/java/com/mycompany/tubes_dpbo/UMKM.java
+++ src/main/java/com/mycompany/tubes_dpbo/UMKM.java
@@ -8,6 +8,31 @@ package com.mycompany.tubes_dpbo;
  *
  * @author applejenius.id
  */
-public class UMKM {
-    
+public class UMKM extends User{
+    private String telephoneNumber;
+    private String[] listKelasDiikuti;
+
+    public UMKM(String telephoneNumber) {
+        this.telephoneNumber = telephoneNumber;
+        this.listKelasDiikuti = new String[0];
+    }
+
+    public void register() {}
+
+    public void login() {}
+
+    public void updateUserData(String newTelephoneNumber) {
+        this.telephoneNumber = newTelephoneNumber;
+    }
+
+    public String getUserInfo() {
+        return "Nomor Telepon: " + telephoneNumber;
+    }
+
+    public void ikutKelas(String namaKelas) {
+        String[] newListKelas = new String[listKelasDiikuti.length + 1];
+        System.arraycopy(listKelasDiikuti, 0, newListKelas, 0, listKelasDiikuti.length);
+        newListKelas[listKelasDiikuti.length] = namaKelas;
+        listKelasDiikuti = newListKelas;
+    }
 }
