227db61cf969fd92ec3ae3ff6bef46fa1a390e81
riyandabella <152076702+riyandabella@users.noreply.github.com>
2024-12-15 20:35:25 +0700

    Update Mentor.java

--- src/main/java/com/mycompany/tubes_dpbo/Mentor.java
+++ src/main/java/com/mycompany/tubes_dpbo/Mentor.java
@@ -8,6 +8,38 @@ package com.mycompany.tubes_dpbo;
  *
  * @author applejenius.id
  */
-public class Mentor {
-    
+class Mentor extends User {
+    private List<String> listKelas;
+    private List<LocalDate> ketersediaan;
+
+    public Mentor(int userId, String nama) {
+        super(userId, nama);
+        this.listKelas = new ArrayList<>();
+        this.ketersediaan = new ArrayList<>();
+    }
+
+    public void addKelas(String kelas) {
+        listKelas.add(kelas);
+        System.out.println("Kelas " + kelas + " berhasil ditambahkan.");
+    }
+
+    public void updateKetersediaan(LocalDate tanggal) {
+        ketersediaan.add(tanggal);
+        System.out.println("Ketersediaan berhasil diupdate pada: " + tanggal);
+    }
+
+    public void showKetersediaan() {
+        System.out.println("Ketersediaan mentor " + super.getNama() + ":");
+        for (LocalDate tanggal : ketersediaan) {
+            System.out.println("- " + tanggal);
+        }
+    }
+
+    public List<String> getListKelas() {
+        return listKelas;
+    }
+
+    public List<LocalDate> getKetersediaan() {
+        return ketersediaan;
+    }
 }
