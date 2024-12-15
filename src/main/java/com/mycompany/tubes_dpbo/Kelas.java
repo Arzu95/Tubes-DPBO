ba27a64b05977168f2edcb31513ed5ceec7e1798
shintaaulya <shintaaulya20@gmail.com>
2024-12-15 23:07:54 +0700

    Update Kelas.java

--- src/main/java/com/mycompany/tubes_dpbo/Kelas.java
+++ src/main/java/com/mycompany/tubes_dpbo/Kelas.java
@@ -1,21 +1,21 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package com.mycompany.tubes_dpbo;
 
 import java.util.ArrayList;
 import java.util.List;
 
 /**
+ * Kelas yang merepresentasikan sebuah pelatihan atau kursus dengan daftar mentor dan materi.
  *
  * @author applejenius.id
  */
 public class Kelas {
+
     private List<Mentor> listMentor;
     private List<String> listMateri;
+    private static final double HARGA_PER_MENTOR = 500000;
+    private static final double HARGA_PER_MATERI = 200000;
+    private static final int MAKSIMUM_KUOTA = 30;
 
-    
     public Kelas(List<Mentor> listMentor, List<String> listMateri) {
         this.listMentor = listMentor;
         this.listMateri = listMateri;
@@ -38,29 +38,21 @@ public class Kelas {
     }
 
     public double hitungHarga() {
-        double hargaPerMentor = 500000;
-        double hargaPerMateri = 200000;
-        return (listMentor.size() * hargaPerMentor) + (listMateri.size() * hargaPerMateri);
+        return (listMentor.size() * HARGA_PER_MENTOR) + (listMateri.size() * HARGA_PER_MATERI);
     }
 
-    public int cekKetersediaanKuota() {
-        int maksimumKuota = 30;
-        int jumlahPeserta = 15;
-        return maksimumKuota - jumlahPeserta;
+    public int cekKetersediaanKuota(int jumlahPeserta) {
+        return Math.max(0, MAKSIMUM_KUOTA - jumlahPeserta);
     }
-
-    public List<UMKM> daftarPeserta() {
-        List<UMKM> listUmkm = new ArrayList<>();
-        listUmkm.add(new UMKM("UMKM A", "Food Industry"));
-        listUmkm.add(new UMKM("UMKM B", "Fashion Industry"));
-        listUmkm.add(new UMKM("UMKM C", "Technology Industry"));
-        return listUmkm;
+    
+    public List<UMKM> daftarPeserta(List<UMKM> listUmkm) {
+        return new ArrayList<>(listUmkm);
     }
-
-    public void displayKelasInfo() {
+    
+    public void displayKelasInfo(int jumlahPeserta, List<UMKM> listUmkm) {
         System.out.println("Daftar Mentor:");
         for (Mentor mentor : listMentor) {
-            System.out.println("- " + mentor.getNamaMentor());
+            System.out.println("- " + mentor);
         }
 
         System.out.println("\nDaftar Materi:");
@@ -69,7 +61,11 @@ public class Kelas {
         }
 
         System.out.println("\nHarga Total: " + hitungHarga());
-        System.out.println("Kuota Tersedia: " + cekKetersediaanKuota());
+        System.out.println("Kuota Tersedia: " + cekKetersediaanKuota(jumlahPeserta));
+
+        System.out.println("\nDaftar Peserta:");
+        for (UMKM umkm : listUmkm) {
+            System.out.println("- " + umkm + " (" + umkm + ")");
+        }
     }
 }
-
