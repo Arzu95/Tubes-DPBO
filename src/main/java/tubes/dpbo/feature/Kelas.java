8bd320c48b5d9d1fa1716fd5d57a9a5cc71f0f5b
shintaaulya <shintaaulya20@gmail.com>
2025-01-02 12:32:11 +0700

    Update Kelas.java

--- src/main/java/tubes/dpbo/feature/Kelas.java
+++ src/main/java/tubes/dpbo/feature/Kelas.java
@@ -1,9 +1,8 @@
-package tubes.dpbo.feature;
+package com.mycompany.tubes_dpbo;
 
 import java.util.ArrayList;
 import java.util.List;
-import tubes.dpbo.user.Mentor;
-import tubes.dpbo.user.UMKM;
+
 /**
  * Kelas yang merepresentasikan sebuah pelatihan atau kursus dengan daftar mentor dan materi.
  *
@@ -11,53 +10,53 @@ import tubes.dpbo.user.UMKM;
  */
 public class Kelas {
 
-    private List<Mentor> listMentor;
-    private List<String> listMateri;
+    private ArrayList<Mentor> daftarMentor;
+    private ArrayList<String> daftarMateri;
     private static final double HARGA_PER_MENTOR = 500000;
     private static final double HARGA_PER_MATERI = 200000;
     private static final int MAKSIMUM_KUOTA = 30;
 
-    public Kelas(List<Mentor> listMentor, List<String> listMateri) {
-        this.listMentor = listMentor;
-        this.listMateri = listMateri;
+    public Kelas(ArrayList<Mentor> daftarMentor, ArrayList<String> daftarMateri) {
+        this.daftarMentor = daftarMentor;
+        this.daftarMateri = daftarMateri;
     }
 
-    public List<Mentor> getListMentor() {
-        return listMentor;
+    public ArrayList<Mentor> getDaftarMentor() {
+        return daftarMentor;
     }
 
-    public void setListMentor(List<Mentor> listMentor) {
-        this.listMentor = listMentor;
+    public void setDaftarMentor(ArrayList<Mentor> daftarMentor) {
+        this.daftarMentor = daftarMentor;
     }
 
-    public List<String> getListMateri() {
-        return listMateri;
+    public ArrayList<String> getDaftarMateri() {
+        return daftarMateri;
     }
 
-    public void setListMateri(List<String> listMateri) {
-        this.listMateri = listMateri;
+    public void setDaftarMateri(ArrayList<String> daftarMateri) {
+        this.daftarMateri = daftarMateri;
     }
 
     public double hitungHarga() {
-        return (listMentor.size() * HARGA_PER_MENTOR) + (listMateri.size() * HARGA_PER_MATERI);
+        return (daftarMentor.size() * HARGA_PER_MENTOR) + (daftarMateri.size() * HARGA_PER_MATERI);
     }
 
     public int cekKetersediaanKuota(int jumlahPeserta) {
         return Math.max(0, MAKSIMUM_KUOTA - jumlahPeserta);
     }
-    
-    public List<UMKM> daftarPeserta(List<UMKM> listUmkm) {
-        return new ArrayList<>(listUmkm);
+
+    public ArrayList<UMKM> daftarPeserta(ArrayList<UMKM> daftarUmkm) {
+        return new ArrayList<>(daftarUmkm);
     }
-    
-    public void displayKelasInfo(int jumlahPeserta, List<UMKM> listUmkm) {
+
+    public void tampilkanInfoKelas(int jumlahPeserta, ArrayList<UMKM> daftarUmkm) {
         System.out.println("Daftar Mentor:");
-        for (Mentor mentor : listMentor) {
+        for (Mentor mentor : daftarMentor) {
             System.out.println("- " + mentor);
         }
 
         System.out.println("\nDaftar Materi:");
-        for (String materi : listMateri) {
+        for (String materi : daftarMateri) {
             System.out.println("- " + materi);
         }
 
@@ -65,8 +64,8 @@ public class Kelas {
         System.out.println("Kuota Tersedia: " + cekKetersediaanKuota(jumlahPeserta));
 
         System.out.println("\nDaftar Peserta:");
-        for (UMKM umkm : listUmkm) {
-            System.out.println("- " + umkm + " (" + umkm + ")");
+        for (UMKM umkm : daftarUmkm) {
+            System.out.println("- " + umkm);
         }
     }
 }
